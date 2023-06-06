c4cf7116154c01db24565f33565c9011ccda39c4
choideakook <shdrnrhd113@naver.com>
2023-06-06 22:42:43 +0900

    docs: study swagger 정리

--- src/main/java/com/baeker/study/study/in/controller/StudyController.java
+++ src/main/java/com/baeker/study/study/in/controller/StudyController.java
@@ -12,6 +12,8 @@ import com.baeker.study.study.in.reqDto.UpdateReqDto;
 import com.baeker.study.study.in.resDto.CreateResDto;
 import com.baeker.study.study.in.resDto.StudyResDto;
 import com.baeker.study.study.in.resDto.UpdateResDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +26,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/study")
 @RequiredArgsConstructor
+@Tag(name = "Study",description = "Study - CRUD")
 public class StudyController {
 
     private final StudyService studyService;
@@ -31,6 +34,7 @@ public class StudyController {
 
     //-- create study --//
     @PostMapping("/v1/create")
+    @Operation(summary = "Study 생성")
     public RsData create(@RequestBody @Valid CreateReqDto dto) {
         log.info("스터디 생성 요청 확인");
 
@@ -43,6 +47,7 @@ public class StudyController {
 
     //-- update name, about, capacity --//
     @PostMapping("/v1/update")
+    @Operation(summary = "업데이트 name / about / capacity")
     public RsData update(@RequestBody @Valid UpdateReqDto dto) {
         log.info("update 요청 확인 id = {}", dto.getId());
 
@@ -53,6 +58,7 @@ public class StudyController {
 
     //-- update leader --//
     @PostMapping("/v1/leader")
+    @Operation(summary = "업데이트 leader")
     public RsData updateLeader(@RequestBody @Valid UpdateLeaderReqDto dto) {
         log.info("리더 수정 요청 확인 id = {}", dto.getId());
 
@@ -64,6 +70,7 @@ public class StudyController {
 
     //-- add xp --//
     @PostMapping("/v1/xp")
+    @Operation(summary = "XP 추가")
     public RsData addXp(@RequestBody @Valid AddXpReqDto dto) {
         log.info("xp 추가 업데이트 요청 확인 id ={}", dto.getId());
 
@@ -75,6 +82,7 @@ public class StudyController {
 
     //-- find list --//
     @GetMapping("/v1/list")
+    @Operation(summary = "모든 스터디 조회 + 페이징")
     public RsData list(@RequestParam @Valid int page) {
         log.info("스터디 리스트 요청 확인 page = {}", page);
 
@@ -89,6 +97,7 @@ public class StudyController {
 
     //-- find by id --//
     @GetMapping("/v1/id")
+    @Operation(summary = "id 로 조회하기")
     public RsData findByName(@RequestParam @Valid Long id) {
         log.info("Study 조회 요청 확인 id = {}", id);
 
@@ -101,6 +110,7 @@ public class StudyController {
 
     //-- find by name --//
     @GetMapping("/v1/name")
+    @Operation(summary = "name 으로 조회하기")
     public RsData findByName(@RequestParam @Valid String name) {
         log.info("Study 조회 요청 확인 name = {}", name);
 
