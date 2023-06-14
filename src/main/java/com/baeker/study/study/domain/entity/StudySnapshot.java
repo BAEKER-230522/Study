b5adc22fc6095d420b993321ec4244a0e8356b13
choideakook <shdrnrhd113@naver.com>
2023-06-14 10:29:57 +0900

    feat: study snapshot 구현 / 이벤트 기반 생성, 업데이트 구현 / 테스트 완료

---
+++ src/main/java/com/baeker/study/study/domain/entity/StudySnapshot.java
@@ -0,0 +1,56 @@
+package com.baeker.study.study.domain.entity;
+
+import com.baeker.study.base.entity.ScoreBase;
+import com.baeker.study.study.in.reqDto.BaekjoonDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = PROTECTED)
+public class StudySnapshot extends ScoreBase {
+
+    private String dayOfWeek;
+
+    @ManyToOne(fetch = LAZY)
+    private Study study;
+
+
+    //-- create snapshot --//
+    public static StudySnapshot create(Study study, BaekjoonDto dto, String dayOfWeek) {
+        StudySnapshot snapshot = StudySnapshot.builder()
+                .study(study)
+                .dayOfWeek(dayOfWeek)
+                .bronze(dto.getBronze())
+                .sliver(dto.getSliver())
+                .gold(dto.getGold())
+                .diamond(dto.getDiamond())
+                .ruby(dto.getRuby())
+                .platinum(dto.getPlatinum())
+                .build();
+
+        study.getSnapshots().add(0, snapshot);
+        return snapshot;
+    }
+
+
+    //-- update snapshot --//
+    public StudySnapshot update(BaekjoonDto dto) {
+        return this.toBuilder()
+                .bronze(this.getBronze() + dto.getBronze())
+                .sliver(this.getSliver() + dto.getSliver())
+                .gold(this.getGold() + dto.getGold())
+                .diamond(this.getDiamond() + dto.getDiamond())
+                .ruby(this.getRuby() + dto.getRuby())
+                .platinum(this.getPlatinum() + dto.getPlatinum())
+                .build();
+    }
+
+}
