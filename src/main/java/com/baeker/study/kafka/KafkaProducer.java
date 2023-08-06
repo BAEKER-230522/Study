469cf4672241eb1b05dda09b3aedc32a5b7b9a1f
I'm back <115536240+choideakook@users.noreply.github.com>
2023-08-06 23:22:55 +0900

    feat: kafka test 추가 (#66)
    * eureka: instance id 추가 / CD : 발동 조건 수정 (#62)
    
    * Feat: 미션 상태 추가
    
    * Feat: studyrule 조회시 문제도 나오도록 설정 TODO: N + 1 해결
    
    * Feat: 테스트 코드 수정
    
    * docs: eureka -> kubernetes 변경 (#51)
    
    * Feat: StudyId 조회 변경
    
    * Feat: Rollback
    
    * eureka 서버 설정 및 카프카
    
    * eureka server connect
    
    * feat: 모든 study 목록 조회 api 구현 (#56)
    
    * feat: feign member ip 업데이트
    
    * Park (#57)
    
    * Feat: eureka server url 변경
    
    * Feat: Deploy 주석삭제
    
    * docs: feing client 이름 service name 으로 변경 (#59)
    
    * eureka: instance id 추가 / CD : 발동 조건 수정 (#61)
    
    * docs: eureka instance id 생성
    
    * docs: cd 조건 수정
    
    ---------
    
    Co-authored-by: PARKPARKWOO <wy9295@naver.com>
    Co-authored-by: 박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
    
    * docs: Kafka 연결
    
    * feat: kafka test 추가
    
    * test: kafka name 추가
    
    ---------
    
    Co-authored-by: PARKPARKWOO <wy9295@naver.com>
    Co-authored-by: 박우영 <118690612+PARKPARKWOO@users.noreply.github.com>

---
+++ src/main/java/com/baeker/study/kafka/KafkaProducer.java
@@ -0,0 +1,27 @@
+package com.baeker.study.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class KafkaProducer {
+
+    @Value("${message.topic.name}")
+    private String topicName;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String msg) {
+        System.out.println(String.format("Produce message : &s", msg));
+        this.kafkaTemplate.send(topicName, msg);
+    }
+}
