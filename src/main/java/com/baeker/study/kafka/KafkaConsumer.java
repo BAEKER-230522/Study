a66af615bab70d34b66bbe92841216dcf8781cd1
I'm back <115536240+choideakook@users.noreply.github.com>
2023-08-08 09:32:21 +0900

    feat: kafka 비활성화 (#68)
    * eureka: instance id 추가 / CD : 발동 조건 수정 (#62)
    
    * Feat: 미션 상태 추가
    
    * Feat: studyrule 조회시 문제도 나오도록 설정 TODO: N + 1 해결
    
    * Feat: 테스트 코드 수정
    
    * docs: eureka -> kubernetes 변경 (#51)
    
    * Feat: StudyId 조회 변경
    
    * Feat: Rollback
    
    * eureka 서버 설정 및 카프카
    
    * eureka server connect
    
    * feat: 모든 study 목록 조회 api 구현 (#56)
    
    * feat: feign member ip 업데이트
    
    * Park (#57)
    
    * Feat: eureka server url 변경
    
    * Feat: Deploy 주석삭제
    
    * docs: feing client 이름 service name 으로 변경 (#59)
    
    * eureka: instance id 추가 / CD : 발동 조건 수정 (#61)
    
    * docs: eureka instance id 생성
    
    * docs: cd 조건 수정
    
    ---------
    
    Co-authored-by: PARKPARKWOO <wy9295@naver.com>
    Co-authored-by: 박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
    
    * docs: Kafka 연결
    
    * feat: kafka test 추가
    
    * test: kafka name 추가
    
    * feat: kafka 비활성화
    
    ---------
    
    Co-authored-by: PARKPARKWOO <wy9295@naver.com>
    Co-authored-by: 박우영 <118690612+PARKPARKWOO@users.noreply.github.com>

--- src/main/java/com/baeker/study/kafka/KafkaConsumer.java
+++ src/main/java/com/baeker/study/kafka/KafkaConsumer.java
@@ -1,84 +1,84 @@
-package com.baeker.study.kafka;
-
-import com.baeker.study.base.exception.NotFoundException;
-import com.baeker.study.domain.studyRule.event.StudyRuleEvent;
-import com.baeker.study.kafka.dto.consume.MemberDto;
-import com.baeker.study.kafka.dto.consume.StudyRuleConsumeDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.Map;
-import java.util.NoSuchElementException;
-
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class KafkaConsumer {
-    private final ApplicationEventPublisher publisher;
-
-    /**
-     * solved message
-     */
-    @KafkaListener(topics = "${message.topic.solved-member}", groupId = ConsumerConfig.GROUP_ID_CONFIG)
-    public void consume(String message) throws IOException {
-        log.debug("#############메시지 대기중 ###############");
-        log.info("message : {}", message);
-        Map<Object, Object> map;
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            map = objectMapper.readValue(message, new TypeReference<Map<Object, Object>>() {
-            });
-        } catch (JsonProcessingException e) {
-            throw new NotFoundException(e.getMessage() + "데이터 없음");
-        }
-        try {
-            Integer memberId = (Integer) map.get("id");
-            Long longId = memberId.longValue();
-            Integer bronze = (Integer) map.get("bronze");
-            Integer silver = (Integer) map.get("silver");
-            Integer gold = (Integer) map.get("gold");
-            Integer platinum = (Integer) map.get("platinum");
-            Integer diamond = (Integer) map.get("diamond");
-            Integer ruby = (Integer) map.get("ruby");
-
-            MemberDto member = new MemberDto(longId, bronze, silver, gold, platinum, diamond, ruby);
-            //TODO: 이벤트리스너
-
-        } catch (NoSuchElementException e) {
-            throw new NotFoundException("Member 데이터 없음");
-        }
-    }
-
-    @KafkaListener(topics = "${message.topic.studyRule}", groupId = ConsumerConfig.GROUP_ID_CONFIG)
-    public void consumeStudyRule(String msg) {
-        Map<Object, Object> map;
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            map = objectMapper.readValue(msg, new TypeReference<Map<Object, Object>>() {
-            });
-        } catch (JsonProcessingException e) {
-            throw new NotFoundException(e.getMessage() + "데이터 없음");
-        }
-        try {
-            Integer studyInt = (Integer) map.get("studyRuleId");
-            Long studyRuleId = studyInt.longValue();
-            //TODO: 필요유무 체크 해야함
-//            Integer ruleInt = (Integer) map.get("ruleId");
-//            Long ruleId = ruleInt.longValue();
-            StudyRuleConsumeDto dto = new StudyRuleConsumeDto(studyRuleId);
-            publisher.publishEvent(new StudyRuleEvent(this, studyRuleId));
-            //TODO: 이벤트 리스너
-        } catch (NoSuchElementException e) {
-            log.error(e.getMessage());
-        }
-    }
-}
-
+//package com.baeker.study.kafka;
+//
+//import com.baeker.study.base.exception.NotFoundException;
+//import com.baeker.study.domain.studyRule.event.StudyRuleEvent;
+//import com.baeker.study.kafka.dto.consume.MemberDto;
+//import com.baeker.study.kafka.dto.consume.StudyRuleConsumeDto;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.core.type.TypeReference;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.springframework.context.ApplicationEventPublisher;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Service;
+//
+//import java.io.IOException;
+//import java.util.Map;
+//import java.util.NoSuchElementException;
+//
+//@Service
+//@Slf4j
+//@RequiredArgsConstructor
+//public class KafkaConsumer {
+//    private final ApplicationEventPublisher publisher;
+//
+//    /**
+//     * solved message
+//     */
+//    @KafkaListener(topics = "${message.topic.solved-member}", groupId = ConsumerConfig.GROUP_ID_CONFIG)
+//    public void consume(String message) throws IOException {
+//        log.debug("#############메시지 대기중 ###############");
+//        log.info("message : {}", message);
+//        Map<Object, Object> map;
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            map = objectMapper.readValue(message, new TypeReference<Map<Object, Object>>() {
+//            });
+//        } catch (JsonProcessingException e) {
+//            throw new NotFoundException(e.getMessage() + "데이터 없음");
+//        }
+//        try {
+//            Integer memberId = (Integer) map.get("id");
+//            Long longId = memberId.longValue();
+//            Integer bronze = (Integer) map.get("bronze");
+//            Integer silver = (Integer) map.get("silver");
+//            Integer gold = (Integer) map.get("gold");
+//            Integer platinum = (Integer) map.get("platinum");
+//            Integer diamond = (Integer) map.get("diamond");
+//            Integer ruby = (Integer) map.get("ruby");
+//
+//            MemberDto member = new MemberDto(longId, bronze, silver, gold, platinum, diamond, ruby);
+//            //TODO: 이벤트리스너
+//
+//        } catch (NoSuchElementException e) {
+//            throw new NotFoundException("Member 데이터 없음");
+//        }
+//    }
+//
+//    @KafkaListener(topics = "${message.topic.studyRule}", groupId = ConsumerConfig.GROUP_ID_CONFIG)
+//    public void consumeStudyRule(String msg) {
+//        Map<Object, Object> map;
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            map = objectMapper.readValue(msg, new TypeReference<Map<Object, Object>>() {
+//            });
+//        } catch (JsonProcessingException e) {
+//            throw new NotFoundException(e.getMessage() + "데이터 없음");
+//        }
+//        try {
+//            Integer studyInt = (Integer) map.get("studyRuleId");
+//            Long studyRuleId = studyInt.longValue();
+//            //TODO: 필요유무 체크 해야함
+////            Integer ruleInt = (Integer) map.get("ruleId");
+////            Long ruleId = ruleInt.longValue();
+//            StudyRuleConsumeDto dto = new StudyRuleConsumeDto(studyRuleId);
+//            publisher.publishEvent(new StudyRuleEvent(this, studyRuleId));
+//            //TODO: 이벤트 리스너
+//        } catch (NoSuchElementException e) {
+//            log.error(e.getMessage());
+//        }
+//    }
+//}
+//
