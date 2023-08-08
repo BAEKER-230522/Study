d65d79f033b62740b97be6c07fb742499f059aef
choideakook <shdrnrhd113@naver.com>
2023-08-08 09:26:06 +0900

    feat: kafka 비활성화

--- src/main/java/com/baeker/study/kafka/KafkaController.java
+++ src/main/java/com/baeker/study/kafka/KafkaController.java
@@ -1,28 +1,28 @@
-package com.baeker.study.kafka;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-@RequestMapping("/kafka")
-public class KafkaController {
-
-    private final KafkaProducer producer;
-
-    @Autowired
-    KafkaController(KafkaProducer producer) {
-        this.producer = producer;
-    }
-
-    @PostMapping("/test")
-    public String sendMsg(@RequestParam String msg) {
-        log.info("message : {}", msg);
-        this.producer.sendMessage(msg);
-        return "success";
-    }
-}
+//package com.baeker.study.kafka;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/kafka")
+//public class KafkaController {
+//
+//    private final KafkaProducer producer;
+//
+//    @Autowired
+//    KafkaController(KafkaProducer producer) {
+//        this.producer = producer;
+//    }
+//
+//    @PostMapping("/test")
+//    public String sendMsg(@RequestParam String msg) {
+//        log.info("message : {}", msg);
+//        this.producer.sendMessage(msg);
+//        return "success";
+//    }
+//}
