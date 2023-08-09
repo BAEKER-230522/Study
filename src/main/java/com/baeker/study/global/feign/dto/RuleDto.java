8c8b99d64ec70325b84e3e8f742b7631e3ec400d
PARKPARKWOO <wy9295@naver.com>
2023-08-09 09:32:40 +0900

    Feat: Dto 형태 변경

--- src/main/java/com/baeker/study/global/feign/dto/RuleDto.java
+++ src/main/java/com/baeker/study/global/feign/dto/RuleDto.java
@@ -15,12 +15,12 @@ public class RuleDto {
     private String name;
     @Schema(description = "Rule 서비스의 소개", example = "소개")
     private String about;
-    @Schema(description = "Rule 서비스의 OJ", example = "BaekJoon")
-    private String provider;
     @Schema(description = "Rule 서비스의 경험치", example = "10")
     private int xp;
     @Schema(description = "Rule 서비스의 문제풀이수(명시된 만큼 문제 풀어야함)", example = "10")
     private int count;
+    @Schema(description = "Rule 서비스의 OJ", example = "BaekJoon")
+    private String provider;
     @Schema(description = "Rule 서비스의 난이도(명시되어있는 난이도의 문제를 풀어야함)",example = "Gold")
     private String difficulty;
 
