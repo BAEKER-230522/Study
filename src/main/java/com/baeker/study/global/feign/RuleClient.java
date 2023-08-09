cd71321133ef3e8000b610b824432628023224f1
PARKPARKWOO <wy9295@naver.com>
2023-08-09 09:58:38 +0900

    Feat: 컴포넌트 어노테이션 삭제

--- src/main/java/com/baeker/study/global/feign/RuleClient.java
+++ src/main/java/com/baeker/study/global/feign/RuleClient.java
@@ -3,11 +3,9 @@ package com.baeker.study.global.feign;
 import com.baeker.study.base.rsdata.RsData;
 import com.baeker.study.global.feign.dto.RuleDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Component
 @FeignClient(name = "rule", url = "${custom.feign.rule}")
 public interface RuleClient {
 
