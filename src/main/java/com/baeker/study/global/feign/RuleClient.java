ec9c1e0a43f7d1d291b088c3a88d5f25fc6b3b5f
PARKPARKWOO <wy9295@naver.com>
2023-08-09 19:48:27 +0900

    Feat: feign 수정

--- src/main/java/com/baeker/study/global/feign/RuleClient.java
+++ src/main/java/com/baeker/study/global/feign/RuleClient.java
@@ -2,13 +2,15 @@ package com.baeker.study.global.feign;
 
 import com.baeker.study.base.rsdata.RsData;
 import com.baeker.study.global.feign.dto.RuleDto;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "rule", url = "${custom.feign.rule}")
+@FeignClient(name = "rule"/*, url = "${custom.feign.rule}"*/)
+@Qualifier
 public interface RuleClient {
 
-    @GetMapping(value = "/v1/search/{ruleId}")
+    @GetMapping(value = "api/rule/v1/search/{ruleId}")
     RsData<RuleDto> getRule(@PathVariable("ruleId") Long ruleId);
 }
