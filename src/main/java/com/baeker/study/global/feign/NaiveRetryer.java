2c841f91effbe03f94a2a56e7d2b4cf13083f9f3
PARKPARKWOO <wy9295@naver.com>
2023-09-09 23:00:08 +0900

    Feat: retry 에러 메시지 추가

--- src/main/java/com/baeker/study/global/feign/NaiveRetryer.java
+++ src/main/java/com/baeker/study/global/feign/NaiveRetryer.java
@@ -2,11 +2,14 @@ package com.baeker.study.global.feign;
 
 import feign.RetryableException;
 import feign.Retryer;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class NaiveRetryer implements Retryer {
     @Override
     public void continueOrPropagate(RetryableException e) {
         try {
+            log.info("Retryer: {}", e.getMessage());
             Thread.sleep(1000L);
         } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
