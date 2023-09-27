26a57381b46057ef6a560bd6b0932fc3615827bd
choideakook <shdrnrhd113@naver.com>
2023-09-27 15:27:06 +0900

    test: study 생성 검증 수정

--- src/test/java/com/baeker/study/study/application/service/StudyCreateServiceTest.java
+++ src/test/java/com/baeker/study/study/application/service/StudyCreateServiceTest.java
@@ -1,24 +1,29 @@
 package com.baeker.study.study.application.service;
 
+import com.baeker.study.global.exception.NoPermissionException;
 import com.baeker.study.myStudy.application.port.in.MyStudyCreateUseCase;
 import com.baeker.study.study.adapter.in.reqDto.StudyCreateReqDto;
+import com.baeker.study.study.application.port.in.SnapshotUseCase;
 import com.baeker.study.study.application.port.out.persistence.StudyRepositoryPort;
 import com.baeker.study.study.domain.entity.Study;
 import com.baeker.study.study.in.resDto.CreateResDto;
 import com.baeker.study.testUtil.MemberClientMock;
+import jakarta.ws.rs.BadRequestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static com.baeker.study.testUtil.CreateStudy.CreateStudy;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,15 +35,23 @@ class StudyCreateServiceTest extends MemberClientMock {
     private StudyRepositoryPort repository;
     @Mock
     private MyStudyCreateUseCase myStudyCreateUseCase;
+    @Mock
+    private SnapshotUseCase snapshotUseCase;
 
     @Test
     @DisplayName("study 생성")
     void no1() {
         Long memberId = 1L;
-
         Study study = createStudy(memberId, "스터디");
+    }
 
-        assertThat(study.getName()).isEqualTo("스터디");
+    @Test
+    @DisplayName("백준 연동이 안된경우")
+    void no2() {
+        Long memberId = 2L;
+        assertThatThrownBy(() ->
+                studyCreateService.study(memberId, null))
+                .isInstanceOf(NoPermissionException.class);
     }
 
     private Study createStudy(Long memberId, String name) {
@@ -47,6 +60,8 @@ class StudyCreateServiceTest extends MemberClientMock {
 
         when(myStudyCreateUseCase.myStudy(eq(memberId), any()))
                 .thenReturn(1L);
+
+        doNothing().when(snapshotUseCase).createSnapshot(any(), any(), anyInt());
 
         StudyCreateReqDto reqDto = new StudyCreateReqDto(name, "about", 10);
         CreateResDto resDto = studyCreateService.study(memberId, reqDto);
--- src/test/java/com/baeker/study/testUtil/MemberClientMock.java
+++ src/test/java/com/baeker/study/testUtil/MemberClientMock.java
@@ -3,10 +3,12 @@ package com.baeker.study.testUtil;
 import com.baeker.study.base.rsdata.RsData;
 import com.baeker.study.global.feign.MemberClient;
 import com.baeker.study.study.in.resDto.MemberResDto;
+import jakarta.ws.rs.BadRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.longThat;
 import static org.mockito.Mockito.when;
 
 public class MemberClientMock {
@@ -19,9 +21,19 @@ public class MemberClientMock {
                 new RsData<>("S-1", "성공", null)
         );
 
-        MemberResDto dto = new MemberResDto("test123", "test123");
-        when(memberClient.findById(any())).thenReturn(
-                new RsData<>("S-1", "성공", dto)
-        );
+        MemberResDto dto = new MemberResDto("test123");
+
+        when(memberClient.findById(any()))
+                .thenAnswer(invocation -> {
+                    Long memberId = (Long) invocation.getArgument(0);
+
+                    if (memberId == 2L)
+                        return new RsData<>("S-1", "성공", dto);
+                    else if (memberId == 3L)
+                        throw new BadRequestException();
+
+                    dto.setBaekJoonName("test123");
+                    return new RsData<>("S-1", "성공", dto);
+                });
     }
 }
