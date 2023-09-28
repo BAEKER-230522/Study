64ccd71482e0761c4d47c980c824c55250188f63
choideakook <shdrnrhd113@naver.com>
2023-09-28 15:04:47 +0900

    test: my study create 테스트 리팩토링

--- src/test/java/com/baeker/study/myStudy/application/service/MyStudyCreateService_createTest.java
+++ src/test/java/com/baeker/study/myStudy/application/service/MyStudyCreateService_createTest.java
@@ -1,6 +1,5 @@
 package com.baeker.study.myStudy.application.service;
 
-import com.baeker.study.myStudy.domain.entity.MyStudy;
 import com.baeker.study.study.domain.entity.Study;
 import com.baeker.study.testUtil.MyStudyCreateMock;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,9 +9,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static com.baeker.study.myStudy.domain.entity.MyStudy.createNewStudy;
 import static com.baeker.study.testUtil.CreateStudy.createStudy;
 
+@DisplayName("My Study 생성")
 @ExtendWith(MockitoExtension.class)
 class MyStudyCreateService_createTest extends MyStudyCreateMock {
 
@@ -21,22 +20,16 @@ class MyStudyCreateService_createTest extends MyStudyCreateMock {
 
     @BeforeEach
     void beforeEach() {
-        memberClientMocking();
         memberClientUpdateMyStudyMocking();
         repoSaveMocking();
     }
 
     @Test
-    @DisplayName("my study 생성")
+    @DisplayName("생성")
     void no1() {
         Long memberId = 1L;
         Study study = createStudy(memberId, 1L, "study");
 
-        Long myStudyId = createMyStudy(memberId, study);
-    }
-
-    private Long createMyStudy(Long memberId, Study study) {
-        MyStudy saveMyStudy = createNewStudy(memberId, study);
-        return myStudyCreateService.myStudy(memberId, study);
+        Long myStudyId = myStudyCreateService.myStudy(memberId, study);
     }
 }
