b70b675c7b30b9aaf34fcd78233ea29b4fd579e5
choideakook <shdrnrhd113@naver.com>
2023-09-28 21:42:43 +0900

    feat: my study join, invite api 생성

---
+++ src/main/java/com/baeker/study/myStudy/adapter/in/reqDto/JoinReqDto.java
@@ -0,0 +1,10 @@
+package com.baeker.study.myStudy.adapter.in.reqDto;
+
+import lombok.Data;
+
+@Data
+public class JoinReqDto {
+
+    private Long studyId;
+    private String msg;
+}
---
+++ src/main/java/com/baeker/study/myStudy/adapter/in/web/MyStudyCreateService.java
@@ -0,0 +1,48 @@
+package com.baeker.study.myStudy.adapter.in.web;
+
+import com.baeker.study.global.jwt.JwtDecrypt;
+import com.baeker.study.myStudy.adapter.in.reqDto.InviteReqDto;
+import com.baeker.study.myStudy.adapter.in.reqDto.JoinReqDto;
+import com.baeker.study.myStudy.application.port.in.MyStudyCreateUseCase;
+import com.baeker.study.study.application.port.in.StudyQueryUseCase;
+import com.baeker.study.study.domain.entity.Study;
+import com.baeker.study.study.in.resDto.CreateResDto;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("${custom.mapping.my-study.web}")
+@RequiredArgsConstructor
+public class MyStudyCreateService {
+
+    private final MyStudyCreateUseCase myStudyCreateUseCase;
+    private final StudyQueryUseCase studyQueryUseCase;
+    private final JwtDecrypt decrypt;
+
+    @Operation(summary = "Study 에 가입 요청")
+    @PostMapping("/v2/join")
+    public ResponseEntity<CreateResDto> join(
+            @RequestHeader("Authorization") String token,
+            @RequestBody @Valid JoinReqDto dto
+    ) {
+        Long memberId = decrypt.getMemberId(token);
+        Study study = studyQueryUseCase.byId(dto.getStudyId());
+        Long myStudyId = myStudyCreateUseCase.join(memberId, study, dto.getMsg());
+        return ResponseEntity.ok(new CreateResDto(study.getId(), myStudyId));
+    }
+
+    @Operation(summary = "Study 로 초대하기")
+    @PostMapping("/v2/invite")
+    public ResponseEntity<CreateResDto> invite(
+            @RequestHeader("Authorization") String token,
+            @RequestBody @Valid InviteReqDto dto
+    ) {
+        Long memberId = decrypt.getMemberId(token);
+        Study study = studyQueryUseCase.byId(dto.getStudyId());
+        Long myStudyId = myStudyCreateUseCase.invite(memberId, study, dto);
+        return ResponseEntity.ok(new CreateResDto(study.getId(), myStudyId));
+    }
+}
