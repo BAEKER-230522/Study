a3dca6f8eb2120fc86a13442341cb8561c99370f
choideakook <shdrnrhd113@naver.com>
2023-10-03 10:44:57 +0900

    feat: my study 삭제, 강퇴 service 생성

--- src/main/java/com/baeker/study/myStudy/application/service/MyStudyDeleteService.java
+++ src/main/java/com/baeker/study/myStudy/application/service/MyStudyDeleteService.java
@@ -1,6 +1,7 @@
 package com.baeker.study.myStudy.application.service;
 
-import com.baeker.study.base.rsdata.RsData;
+import com.baeker.study.global.exception.NoPermissionException;
+import com.baeker.study.global.exception.NotFoundException;
 import com.baeker.study.global.feign.MemberClient;
 import com.baeker.study.myStudy.application.port.in.MyStudyDeleteUseCase;
 import com.baeker.study.myStudy.application.port.out.persistence.MyStudyRepositoryPort;
@@ -9,6 +10,8 @@ import com.baeker.study.myStudy.out.reqDto.DeleteMyStudyReqDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.baeker.study.myStudy.domain.entity.StudyStatus.MEMBER;
 
 @Service
 @Transactional
@@ -20,17 +23,48 @@ public class MyStudyDeleteService implements MyStudyDeleteUseCase {
 
     @Override
     public void myStudy(Long memberId, MyStudy myStudy) {
-
-
+        if (myStudy.getStatus() != MEMBER)
+            pendingPermissionCheck(memberId, myStudy);
+        else
+            memberPermissionCheck(memberId, myStudy);
 
         deleteMember(myStudy);
     }
 
+    private void pendingPermissionCheck(Long memberId, MyStudy myStudy) {
+        if (
+                memberId == myStudy.getMember() ||
+                memberId == myStudy.getStudy().getLeader()
+        )
+            return;
+
+        throw new NoPermissionException("권한이 없습니다.");
+    }
+
+    private void memberPermissionCheck(Long memberId, MyStudy myStudy) {
+        if (memberId == myStudy.getStudy().getLeader())
+            throw new NoPermissionException("스터디 장은 스터디를 탈퇴할 수 없습니다.");
+
+        if (memberId != myStudy.getMember())
+            throw new NoPermissionException("권한이 없습니다.");
+    }
+
     @Override
     public void dropOut(Long memberId, MyStudy myStudy) {
-
+        isMember(myStudy);
+        isLeader(memberId, myStudy);
 
         deleteMember(myStudy);
+    }
+
+    private void isMember(MyStudy myStudy) {
+        if (myStudy.getStatus() != MEMBER)
+            throw new NotFoundException("스터디의 정회원이 아닙니다.");
+    }
+
+    private void isLeader(Long memberId, MyStudy myStudy) {
+        if (memberId != myStudy.getStudy().getLeader())
+            throw new NoPermissionException("권한이 없습니다.");
     }
 
     private void deleteMember(MyStudy myStudy) {
