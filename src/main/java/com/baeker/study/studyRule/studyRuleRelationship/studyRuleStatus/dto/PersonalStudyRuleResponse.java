8c7615b8ad82c397e8d28d62087ee8e590096245
PARKPARKWOO <wy9295@naver.com>
2023-10-30 16:22:37 +0900

    Feat 미션 조회시 memberId 도 응답

--- src/main/java/com/baeker/study/studyRule/studyRuleRelationship/studyRuleStatus/dto/PersonalStudyRuleResponse.java
+++ src/main/java/com/baeker/study/studyRule/studyRuleRelationship/studyRuleStatus/dto/PersonalStudyRuleResponse.java
@@ -5,9 +5,10 @@ import com.baeker.study.studyRule.studyRuleRelationship.problemStatus.dto.Proble
 
 import java.util.List;
 
-public record PersonalStudyRuleResponse(String nickName, Status personalStudyRuleStatus,
-                                        List<ProblemStatusQueryDto> problemStatusQueryDtos) {
-    public PersonalStudyRuleResponse(String nickName, PersonalStudyRuleDto ruleDto) {
-        this(nickName, ruleDto.personalStudyRuleStatus(), ruleDto.problemStatusQueryDtos());
+public record PersonalStudyRuleResponse(String nickName, String memberId, Status personalStudyRuleStatus,
+                                        List<ProblemStatusQueryDto> problemStatusQueryDtos
+) {
+    public PersonalStudyRuleResponse(String nickName, String memberId, PersonalStudyRuleDto ruleDto) {
+        this(nickName,memberId, ruleDto.personalStudyRuleStatus(), ruleDto.problemStatusQueryDtos());
     }
 }
